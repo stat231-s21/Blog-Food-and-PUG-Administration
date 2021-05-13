72ba9c7c57c2b1963144a1c1a2d06d016c706b58
sabrinatrombetta <strombetta21@amherst.edu>
2021-05-13 10:08:25 -0400

    adding May 10 data, adding name and title to index file

--- raw data/sabrina/KFF_vax_wrangling.R
+++ raw data/sabrina/KFF_vax_wrangling.R
@@ -359,6 +359,47 @@ KFF_vax_May3 <- read_csv("KFF_vax_May3.csv",
            native_metric = native_vax/native_pop,
            pacific_islander_metric = pacific_islander_vax/pacific_islander_pop)
 
+## May 10
+KFF_vax_May10 <- read_csv("KFF_vax_May10.csv", 
+                         col_types = cols(Footnotes = col_character()), 
+                         skip = 2) %>%
+  slice(1:51) %>%
+  clean_names() %>%
+  rename("state" = "location", 
+         "pacific_islander_vax" = "native_hawaiian_or_other_pacific_islander_percent_of_vaccinations", 
+         "native_vax" = "american_indian_or_alaska_native_percent_of_vaccinations", 
+         "white_vax" = "white_percent_of_vaccinations", 
+         "black_vax" = "black_percent_of_vaccinations", 
+         "hispanic_vax" = "hispanic_percent_of_vaccinations", 
+         "asian_vax" = "asian_percent_of_vaccinations") %>%
+  select(-c(footnotes)) %>%
+  mutate(asian_vax = ifelse(asian_vax == "<.01", "0", asian_vax), # replacing <0.01 with 0
+         black_vax = ifelse(black_vax == "<.01", "0", black_vax),
+         native_vax = ifelse(native_vax == "<.01", "0", native_vax),
+         pacific_islander_vax= ifelse(pacific_islander_vax == "<.01", "0", 
+                                      pacific_islander_vax)) %>%
+  replace_with_na_at(.vars = c("white_vax",
+                               "asian_vax", 
+                               "black_vax",
+                               "hispanic_vax",
+                               "native_vax",
+                               "pacific_islander_vax"),
+                     condition = ~ .x == "NR") %>% # replace NR (not reported) with NA
+  mutate(white_vax = as.numeric(white_vax), # converting from chr to num
+         asian_vax = as.numeric(asian_vax),
+         black_vax = as.numeric(black_vax),
+         hispanic_vax = as.numeric(hispanic_vax),
+         native_vax = as.numeric(native_vax),
+         pacific_islander_vax = as.numeric(pacific_islander_vax), 
+         date = "May 3") %>%
+  inner_join(state_demographics, by = "state") %>%
+  mutate(white_metric = white_vax/white_pop,
+         black_metric = black_vax/black_pop,
+         hispanic_metric = hispanic_vax/hispanic_pop, 
+         asian_metric = asian_vax/asian_pop,
+         native_metric = native_vax/native_pop,
+         pacific_islander_metric = pacific_islander_vax/pacific_islander_pop)
+
 # join each KFF_vax_[date] so that the column names are the same, want to 
 # be able to use reactive dataset in the shiny app to filter for the date the
 # user chooses. 
@@ -370,7 +411,8 @@ KFF_vax_join <- KFF_vax_March1 %>%
   rbind(KFF_vax_April12) %>%
   rbind(KFF_vax_April19) %>%
   rbind(KFF_vax_April26) %>%
-  rbind(KFF_vax_May3)
+  rbind(KFF_vax_May3) %>%
+  rbind(KFF_vax_May10)
 
 #### DATA WRANGLING FOR HEXBIN MAP 
 
